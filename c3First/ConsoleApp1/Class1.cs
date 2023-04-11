c1f5de9847441c9c74c1e501393c299c54e07861
Larmesdesang <85700200+GeplLoL@users.noreply.github.com>
2023-04-11 11:09:43 +0300

    Add files via upload

---
+++ c3First/ConsoleApp1/Class1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Class1
+    {
+        public static void Main(string[] args)
+        {
+            string nimi = "Python";
+            Class2.Tere(nimi);
+            float a = 10;
+            float b = 20;
+            int n = 3;
+            char z = '/';
+            float vastus = Class2.Korruta(a, b, n);
+            float vastuCalc = Class2.Calc(a, b, z);
+            Console.WriteLine(vastuCalc);
+            Console.WriteLine(vastus);
+            Class2.Zvezd();
+            /*
+            Random rnd = new Random();
+            int n = rnd.Next(-100, 100);
+            int m = rnd.Next(-100, 100);
+            Console.WriteLine(n + "," + m);
+            int[] arr = new int[0];
+            if (n > m)
+            {
+                while (n != m)
+                {
+                    m++;
+                    Array.Resize(ref arr, arr.Length + 1);
+                    arr[arr.Length - 1] = m;
+                    Console.WriteLine(m * m);
+                }
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    Console.Write(arr[i] + " ");
+                }
+            }
+            else if (m > n)
+            {
+                while (m != n)
+                {
+                    n++;
+                    Array.Resize(ref arr, arr.Length + 1);
+                    arr[arr.Length - 1] = n;
+                    Console.WriteLine(n*n);
+                }
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    Console.Write(arr[i] + " ");
+                }
+            }
+            int sumAR = 0;
+            int[] arrNum = new int[0];
+            for (int i = 1; i < 5; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                Array.Resize(ref arrNum, arrNum.Length + 1);
+                arrNum[arrNum.Length - 1] = num;
+            for (int j = 2; j < 5; j++)
+            {
+                sumAR = arrNum[i] + arrNum[j];
+            }
+            Console.WriteLine(sumAR);
+            }*/
+        }
+    }
+}
---
+++ c3First/ConsoleApp1/Class2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Class2
+    {
+        public static void Tere(string name) 
+        {
+            Console.WriteLine("Tere {0}", name);
+        }
+        public static float Korruta(float arv1, float arv2, int n)
+        {
+            return (arv1 + arv2)/n;
+        }
+        public static float Calc(float arv1, float arv2, char znak)
+        {
+            float vastus = 0;
+            if (znak=='+')
+            {
+                vastus = arv1 + arv2;
+            }
+            else if (znak=='-')
+            {
+                vastus = arv1 - arv2;
+            }
+            else if (znak=='*')
+            {
+                vastus = arv1 * arv2;
+            }
+            else if (znak=='/')
+            {
+                vastus=arv1 / arv2;
+                if (vastus==0)
+                {
+                    Console.WriteLine("error");
+                }
+            }
+            for (int i = 0; i < Math.Abs(vastus); i++)
+            {
+                Console.Write("nurr ");
+            }
+            Console.WriteLine();
+            return vastus;
+        }
+        public static void Zvezd()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine();
+                int n = int.Parse(Console.ReadLine());
+                for (int j = 0; j < n; j++)
+                    {
+                        Console.Write('*');
+                    }
+            }
+        }
+    }
+}
